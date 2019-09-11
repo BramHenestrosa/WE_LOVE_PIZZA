286d6eae4ab1c12d1e714954909db36639911742
Abraham Gonzalez Henestrosa <Abraham@Abrahams-MacBook-Pro.local>
2019-09-10 21:51:44 -0500

    modifique esta vaina

--- mainpizza.js
+++ mainpizza.js
@@ -1,28 +1,43 @@
+
+//inputs
 let nombre = document.getElementById("nombre")
 let telefono = document.getElementById("telefono")
 let direccion = document.getElementById("direccion")
 let ingrediente = document.getElementById("ingrediente")
 
+//containers
+let pepe = document.getElementById("pepe")
 
+//button
 let accion = document.getElementById("accion")
 
 const datos = () => {
 
-nombreText.innerHTML=nombre.value;
-telefonoText.innerHTML=telefono.value;
-direccionText.innerHTML=direccion.value;
+    nombreText.innerHTML=nombre.value;
+    telefonoText.innerHTML=telefono.value;
+    direccionText.innerHTML=direccion.value;
 
-// let resultPizza = pizza.value;
-// let pizzaFinal = resultPizza.toUpperCase();
+    let resultPizza = ingrediente.value;
+    let pizzaFinal = resultPizza.toUpperCase();
+
+    // console.log(pizzaFinal);
+
+    if (pizzaFinal === "PEPERONI") {
+        pepe.classList.remove("d-none")
+
+    } else if (pizzaFinal === "PIÑA") {
+        hawa.classList.remove("d-none")
 
 
+    }else if (pizzaFinal === "ACEITUNAS"){
+        aceituna.classList.remove("d-none")
 
+    } else {
 
+        console.log("nojala")
 
-
-
+    }
 
 }
 
-
-accion.addEventListener ("click", datos)
+accion.addEventListener("click", datos)
